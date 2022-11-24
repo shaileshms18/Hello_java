58e3f9341fdce69ecb2846446d0801e7b71326bf
Shailesh MS <shaileshms18@gmail.com>
2022-11-24 09:40:50 +0530

    update1

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,5 @@
 public class Helloworld{
   public static void main(String[] args){
-    System.out.println("Hello,world update from developer1");
+    System.out.println("Hello,world update from developer");
   }
 }
